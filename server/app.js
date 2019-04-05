ac0aa1230439faf8f8ae3ff06fbdb98d01da35e2
shaktigurung <mr.shakti.gurung@gmail.com>
2019-04-05 14:50:22 +1100

    Schema and Database connection

--- server/app.js
+++ server/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = 4000;
+
+// connect to database
+mongoose.connect('mongodb+srv://shakti:Decoder35@cluster0-em4nn.mongodb.net/test?retryWrites=true', {useNewUrlParser: true});
+mongoose.connection.once('open', ()=> {
+    console.log('connected to database');
+});
 
 app.use('/graphql', graphqlHTTP({
     //schema
